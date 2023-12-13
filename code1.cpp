23db7e1417230d4668ac4621fc3f71a45d4db603
Aditya Jambhale <jambhaleaditya91@gmail.com>
2023-12-13 11:52:48 +0530

    git command practice

---
+++ code1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+   
+    std::cout<< "which country do you live in?" << std::endl;
+    std::string country;
+    std::cin>>country;
+    std::cout << "I have heard great things about" " " << country << " ""i would like to go sometime"<<std::endl;
+    return 0;
+}
